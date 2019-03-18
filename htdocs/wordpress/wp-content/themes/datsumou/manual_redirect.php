fa56c4f78276732d9ff503d960314926c90e5829
tomokazu <tomokazu_1011@yahoo.co.jp>
2019-03-19 03:21:59 +0900

    190318home

--- htdocs/wordpress/wp-content/themes/datsumou/manual_redirect.php
+++ htdocs/wordpress/wp-content/themes/datsumou/manual_redirect.php
@@ -3,8 +3,12 @@
 	<div class="card">
 		<h2>設定ページ</h2>
 		<p>「固定ページ」の「リダイレクト」の子ページになります。</p>
-		<p>リダイレクト先はページ編集画面の「Redirect / Destination URL:」に入力してください。<br>
+		<p>リダイレクト先はページ編集画面の「リダイレクト URL:」に入力してください。<br>
 		固定ページのURL（https://脱毛診断メーカー.com/link/○○）にアクセスするとリダイレクト先に設定したページへ自動で飛びます。</p>
+		<p>パラメーターもリダイレクト先に引き継げます。<br>
+		例）https://xn--lckwf7cb5558dg0hnt1bzdp.com/link/musee/?<span style="color:red">ref=1111</span><br>
+		↓<br>
+		https://t.82comb.net/cl?ak=1CK8X.3.5J3QD&<span style="color:red">ref=1111</span></p>
 
 	</div>
 </div>

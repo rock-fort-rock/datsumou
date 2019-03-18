a44cefa2e2eb7547d5a87daf0383e7d9f1dc542e
tomokazu <tomokazu_1011@yahoo.co.jp>
2019-03-18 19:51:38 +0900

    リダイレクト設定

--- htdocs/wordpress/wp-content/themes/datsumou/page-redirect.php
+++ htdocs/wordpress/wp-content/themes/datsumou/page-redirect.php
@@ -5,6 +5,26 @@ Template Name: リダイレクト用
 $status = [
 	'id' => 'redirect',
 ];
+$url = parse_url(get_field('redirect_url'));
+// print_r($url);
+$para = '';
+if(!empty($_SERVER['QUERY_STRING'])){//URLにパラメータあり
+	if(!empty($url['query'])){//設定リンクにパラメータあり
+		$para = '?'.$url['query'].'&'.$_SERVER['QUERY_STRING'];
+	}else{
+		$para = '?'.$_SERVER['QUERY_STRING'];
+	}
+}else{//URLにパラメータなし
+	if(!empty($url['query'])){//設定リンクにパラメータあり
+		$para = '?'.$url['query'];
+	}
+}
+$redirect = $url['scheme'] . '://' . $url['host'] . $url['path'] . $para;
+// echo $redirect;
+//exit();
+if(get_field('redirect_url')){
+	header("Location: $redirect");
+}
 ?>
 <?php get_header(); ?>
 

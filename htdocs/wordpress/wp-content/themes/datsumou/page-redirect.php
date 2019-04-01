86f6320b8e73637529aaa692292843353a0e4440
tomokazu <tomokazu_1011@yahoo.co.jp>
2019-04-02 00:52:16 +0900

    190401home

--- htdocs/wordpress/wp-content/themes/datsumou/page-redirect.php
+++ htdocs/wordpress/wp-content/themes/datsumou/page-redirect.php
@@ -5,24 +5,23 @@ Template Name: リダイレクト用
 $status = [
 	'id' => 'redirect',
 ];
-$url = parse_url(get_field('redirect_url'));
-// print_r($url);
-$para = '';
-if(!empty($_SERVER['QUERY_STRING'])){//URLにパラメータあり
-	if(!empty($url['query'])){//設定リンクにパラメータあり
-		$para = '?'.$url['query'].'&'.$_SERVER['QUERY_STRING'];
-	}else{
-		$para = '?'.$_SERVER['QUERY_STRING'];
-	}
-}else{//URLにパラメータなし
-	if(!empty($url['query'])){//設定リンクにパラメータあり
-		$para = '?'.$url['query'];
-	}
-}
-$redirect = $url['scheme'] . '://' . $url['host'] . $url['path'] . $para;
-// echo $redirect;
-//exit();
 if(get_field('redirect_url')){
+	$url = parse_url(get_field('redirect_url'));
+	// print_r($url);
+	$para = '';
+	if(!empty($_SERVER['QUERY_STRING'])){//URLにパラメータあり
+		if(!empty($url['query'])){//設定リンクにパラメータあり
+			$para = '?'.$url['query'].'&'.$_SERVER['QUERY_STRING'];
+		}else{
+			$para = '?'.$_SERVER['QUERY_STRING'];
+		}
+	}else{//URLにパラメータなし
+		if(!empty($url['query'])){//設定リンクにパラメータあり
+			$para = '?'.$url['query'];
+		}
+	}
+	$redirect = $url['scheme'] . '://' . $url['host'] . $url['path'] . $para;
+
 	header("Location: $redirect");
 }
 ?>
@@ -31,6 +30,12 @@ if(get_field('redirect_url')){
 <div class="mainContents">
 	<section class="contentBlock">
 		<?php the_title(); ?>
+		<?php the_post(); the_content(); ?>
+		<?php
+		// $imgObj = get_field('redirect_image');
+		// $img = $imgObj['sizes']['medium'];
+		?>
+		<!-- <img src="<?php echo $img; ?>"> -->
 	</section>
 </div>
 
